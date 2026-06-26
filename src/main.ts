38ab32cd78c73bd348d134db8c60f4af95773641
Yehuda Hason <yehudaxt@gmail.com>
2026-06-26 15:47:37 +0300

    final

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,45 @@
 const slider = document.getElementById("slider") as HTMLInputElement | null;
+const price_amount = document.getElementById(
+  "price_amount",
+) as HTMLElement | null;
+
+const checkbox = document.getElementById("checkbox") as HTMLInputElement;
+const pageviewsEl = document.getElementById("pageviews") as HTMLElement;
+
+const monthlyValues = [4, 8, 16, 24, 36];
+const yearlyValues = [3, 6, 12, 22, 30];
+const pageviews = ["10K", "50K", "100K", "500K", "1M"];
 
 function updateSlider() {
-  if (slider) {
-    const min = Number(slider.min);
-    const max = Number(slider.max);
-    const value = Number(slider.value);
-    let percent = ((value - min) / (max - min)) * 100;
+  if (!slider) return;
+  if (!pageviews) return;
 
-    slider.style.background = `linear-gradient(
-      to right,
-      hsl(174, 77%, 80%) 0%,
-      hsl(174, 77%, 80%) ${percent}%,
-      hsl(224, 65%, 95%) ${percent}%,
-      hsl(224, 65%, 95%) 100%
-    )`;
+  const min = Number(slider.min);
+  const max = Number(slider.max);
+  const value = Number(slider.value);
 
-    console.log("update", percent);
+  const percent = ((value - min) / (max - min)) * 100;
+  const arr = checkbox.checked ? yearlyValues : monthlyValues;
+  const price = arr[value];
+  const views = pageviews[value];
+  pageviewsEl.innerHTML = views + " PAGEVIEWS";
+
+  if (price_amount) {
+    price_amount.textContent = `$${price}`;
   }
+
+  slider.style.background = `linear-gradient(
+    to right,
+    hsl(174, 77%, 80%) 0%,
+    hsl(174, 77%, 80%) ${percent}%,
+    hsl(224, 65%, 95%) ${percent}%,
+    hsl(224, 65%, 95%) 100%
+  )`;
 }
 
 if (slider) {
   updateSlider();
+
   slider.addEventListener("input", updateSlider);
+  checkbox.addEventListener("change", updateSlider);
 }
